e116053fbd15a82bc9cc05d82107f6a6a40c7880
owen151128 <owen151128@gmail.com>
2026-05-20 07:04:21 +0900

    feat(basics): Study state in compose

--- basic-codelab/app/src/main/java/kr/owens/basic/codelab/MainActivity.kt
+++ basic-codelab/app/src/main/java/kr/owens/basic/codelab/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,23 +43,29 @@ fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier.padding(24.dp)) {
-            Column(modifier.weight(1f)) {
+            Column(
+                modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text("Hello ")
                 Text(name)
             }
-            ElevatedButton(onClick = { /* TODO */ }) {
-                Text("Show more")
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
 @Composable
 fun GreetingPreview() {
     BasicCodelabTheme {
