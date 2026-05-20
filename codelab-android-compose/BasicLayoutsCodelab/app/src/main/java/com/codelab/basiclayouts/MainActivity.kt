3f9a71207ecc5bfd476525e8dc9b37f0f203ac88
owen151128 <owen151128@gmail.com>
2026-05-20 21:40:11 +0900

    feat(mysoothe): Study mySoothe App

--- codelab-android-compose/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ codelab-android-compose/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -50,6 +50,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -242,7 +243,11 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 // Step: MySoothe App - Scaffold
 @Composable
 fun MySootheAppPortrait() {
-    // Implement composable here
+    MySootheTheme {
+        Scaffold(bottomBar = { SootheBottomNavigation() }) {
+            HomeScreen(Modifier.padding(it))
+        }
+    }
 }
 
 // Step: Bottom navigation - Material
