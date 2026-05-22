5bc70e40456090a8113dd9c20663055b2c51e1e2
owen151128 <owen151128@gmail.com>
2026-05-22 11:58:00 +0900

    feat(state): Study events in Compose

--- BasicStateCodelab/app/src/main/java/kr/owens/basic/state/codelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/kr/owens/basic/state/codelab/WaterCounter.kt
@@ -1,6 +1,8 @@
 package kr.owens.basic.state.codelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,6 +10,11 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text("You' ve had $count classes.", modifier.padding(16.dp))
+    Column(modifier.padding(16.dp)) {
+        var count = 0
+        Text("You' ve had $count classes.", modifier.padding(16.dp))
+        Button({ count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
