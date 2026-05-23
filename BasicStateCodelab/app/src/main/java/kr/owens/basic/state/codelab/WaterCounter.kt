2a32cdddf32d2fadc3da84500f01d77e83992e29
owen151128 <owen151128@gmail.com>
2026-05-24 02:58:21 +0900

    feat(state): Study restore state in Compose

--- BasicStateCodelab/app/src/main/java/kr/owens/basic/state/codelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/kr/owens/basic/state/codelab/WaterCounter.kt
@@ -7,8 +7,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -16,7 +18,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableIntStateOf(0) }
         if (count > 0) {
             var showTask by remember { mutableStateOf(true) }
             if (showTask) {
