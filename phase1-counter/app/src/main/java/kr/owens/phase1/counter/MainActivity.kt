2c223506812bae8c0d13620c33a9bb7dfcd11b90
owen151128 <owen151128@gmail.com>
2026-06-04 21:26:18 +0900

    feat(counter): Implements counter

--- phase1-counter/app/src/main/java/kr/owens/phase1/counter/MainActivity.kt
+++ phase1-counter/app/src/main/java/kr/owens/phase1/counter/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import kr.owens.phase1.counter.ui.theme.Phase1counterTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Phase1counterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Counter(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +46,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Counter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableIntStateOf(0) }
+    Counter(count, { count++ }, { count-- }, { count = 0 }, modifier)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Counter(
+    count: Int,
+    onIncrement: () -> Unit,
+    onDecrement: () -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(modifier) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Spacer(Modifier.weight(1f))
+            Text(
+                text = "Count : $count",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .weight(0.5f)
+            ) {
+                Button(onDecrement, Modifier.weight(1f)) { Text("-") }
+                Spacer(Modifier.weight(0.1f))
+                Button(onIncrement, Modifier.weight(1f)) { Text("+") }
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.5f)
+                    .padding(horizontal = 8.dp)
+            ) {
+                Button(onReset, Modifier.fillMaxWidth()) { Text("Reset") }
+            }
+        }
+    }
+}
+
+class CountProvider : PreviewParameterProvider<Int> {
+    override val values = sequenceOf(0, 7, 999_999)
+}
+
+@PreviewLightDark
+@Composable
+fun CounterPreview(@PreviewParameter(CountProvider::class) count: Int) {
     Phase1counterTheme {
-        Greeting("Android")
+        Counter(count, {}, {}, {})
     }
 }
